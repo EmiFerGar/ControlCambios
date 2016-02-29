bd6416339472f291df0d9675dbe7fb84eba74f92
Profesor <Profesor@dam100>
2016-02-29 10:10:25 +0100

    Añadido un System

--- src/controlcambiosgit/ControlCambiosGit.java
+++ src/controlcambiosgit/ControlCambiosGit.java
@@ -6,11 +6,11 @@ package controlcambiosgit;
  */
 public class ControlCambiosGit {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
     	System.out.println("Primer Comentario");
+        
+        System.out.println("Pruebas CVS");
     }
     
 }
